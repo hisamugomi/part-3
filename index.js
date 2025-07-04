18a61af7a82afa4ebb7396419e876fc9e0855259
Hisamu Gomi <hisamu.gomi@uleth.ca>
2025-07-04 16:32:51 +0900

    part3.4

--- index.js
+++ index.js
@@ -46,6 +46,11 @@ app.get('/api/persons/:id', (request, response) => {
   response.json(person)
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)    
+    // console.log(persons)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
